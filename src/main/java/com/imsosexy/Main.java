e77b15cd8cccbb0d679c710d0c4398ef52e5bab8
홍석현 <sh.hong@steg.co.kr>
2022-03-03 16:57:29 +0900

    Initial commit

---
+++ src/main/java/com/imsosexy/Main.java
@@ -0,0 +1,12 @@
+package com.imsosexy;
+
+public class Main {
+    public static void main(String[] args) {
+        String hi = "Hi";
+        System.out.println(hi);
+        System.out.println("Hi");
+
+        System.out.println("Hi2");
+
+    }
+}
---
+++ src/main/java/com/imsosexy/main2.java
@@ -0,0 +1,8 @@
+package com.imsosexy;
+
+public class main2 {
+    public static void main(String[] args) {
+        System.out.println("hi everyone");
+
+    }
+}
