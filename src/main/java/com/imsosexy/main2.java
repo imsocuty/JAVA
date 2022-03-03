5369d2b9e36c1f3723bd5ada2c5523058db12ea8
홍석현 <sh.hong@steg.co.kr>
2022-03-03 16:59:24 +0900

    Changes

--- src/main/java/com/imsosexy/main2.java
+++ src/main/java/com/imsosexy/main2.java
@@ -2,7 +2,7 @@ package com.imsosexy;
 
 public class main2 {
     public static void main(String[] args) {
-        System.out.println("hi everyone");
+        System.out.println("hi everyone11");
 
     }
 }
